fb9d40cf83a3886120797b63a53e5c7954012995
Mai Anh Kha <maianhkha.dev@gmail.com>
2018-09-26 14:11:38 +0700

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,63 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  $('.section-07 .slick').slick({
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 3
+  $('.section-01 .slick').slick({
+    dots: false,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true
+  })
+
+  $('.section-03 .slick').slick({
+    dots: false,
+    arrows: false,
+    variableWidth: true
+  })
+
+  let dots = document.querySelectorAll('.section-03 svg .dot')
+  dots.forEach(function(dot) {
+    dot.addEventListener('click', function() {
+
+      let popovers = document.querySelectorAll('.section-03 svg .popover')
+      popovers.forEach(function(popover) {
+        popover.classList.remove('show')
+      })
+
+      let popover = dot.previousElementSibling
+      popover.classList.add('show')
+    })
+  })
+
+  // Set active color
+  // let color = document.querySelector('.section-06 .block-colors figure')
+  // color.classList.add('active')
+  //
+  // let colors = document.querySelectorAll('.section-06 .block-colors figure')
+  // dots.forEach(function(dot) {
+  // })
+
+  //init same height
+  let elements = document.querySelectorAll('[data-same-height-with]')
+  elements.forEach(function(element) {
+    element.dataset.fdafa = true
+  })
+  elements.forEach(function(element) {
+
+    if(element.dataset.fdafa === "true") {
+
+      let name = element.dataset.sameHeightWith
+      let doms = document.querySelectorAll(`[data-same-height-with="${name}"]`)
+      console.log(doms)
+      let maxHeight = 0;
+      doms.forEach(function(dom) {
+        if(dom.clientHeight > maxHeight) {
+          maxHeight = dom.clientHeight
+        }
+      })
+
+      doms.forEach(function(dom) {
+        dom.style.height = `${maxHeight}px`
+        dom.dataset.fdafa = false
+      })
+    }
   })
 })
