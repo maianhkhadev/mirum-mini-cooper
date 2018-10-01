001989c09ab49db89771dd5190abbb6784f97f51
Mai Anh Kha <maianhkha.dev@gmail.com>
2018-10-01 15:46:00 +0700

    modal ipad

--- app/scripts/youtube-video.js
+++ app/scripts/youtube-video.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   var tag = document.createElement('script')
-  tag.src = "https://www.youtube.com/iframe_api"
+  tag.src = 'https://www.youtube.com/iframe_api'
   var firstScriptTag = document.getElementsByTagName('script')[0]
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
